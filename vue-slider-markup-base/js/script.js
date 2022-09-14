d44e28df63074d482bc0b6d83316b4d2c2c272ea
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-09-14 11:01:15 +0200

    push

--- vue-slider-markup-base/js/script.js
+++ vue-slider-markup-base/js/script.js
@@ -1,43 +1,87 @@
 console.log('JS OK')
 
-const root = new Vue({
-    el: '#app',
-    data: {
-        slides: [
-            {
-                image: 'img/01.jpg',
-                title: 'Svezia',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-            },
-            {
-                image: 'img/02.jpg',
-                title: 'Svizzera',
-                text: 'Lorem ipsum.',
-            },
-            {
-                image: 'img/03.jpg',
-                title: 'Gran Bretagna',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-            },
-            {
-                image: 'img/04.jpg',
-                title: 'Germania',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-            },
-            {
-                image: 'img/05.jpg',
-                title: 'Paradise',
-                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-            }],
-        activeClass: '',
-        index: 0
+const slides = [
+    {
+        image: 'img/01.jpg',
+        title: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
     },
-    methods: {
-        moveImg() {
-            this.index++
+    {
+        image: 'img/02.jpg',
+        title: 'Svizzera',
+        text: 'Lorem ipsum.',
+    },
+    {
+        image: 'img/03.jpg',
+        title: 'Gran Bretagna',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+        image: 'img/04.jpg',
+        title: 'Germania',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+    },
+    {
+        image: 'img/05.jpg',
+        title: 'Paradise',
+        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+    }
+];
+
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            activeIndex: 0,
+            slides,
+            intervalId: undefined
+        },
+
+        mounted() {
+            setInterval(this.showNext, 3000);
+        },
+
+        methods: {
+            createInterval() {
+                this.intervalId = setInterval(this.showNext, 3000);
+            },
+            stopInterval() {
+                if (this.intervalId) {
+                    clearInterval(this.intervalId);
+                }
+            },
+
+            showPrev: function () {
+                if (this.activeIndex > 0) {
+                    this.activeIndex--;
+                } else {
+                    this.activeIndex = this.activeIndex.lenght - 1;
+                }
+            },
+            showNext: function () {
+                if (this.activeIndex === this.activeIndex.lenght - 1) {
+                    this.activeIndex = 0;
+                } else {
+                    this.activeIndex++;
+                }
+            },
+            onNextClick() {
+                this.showNext();
+                this.stopInterval();
+                this.createInterval();
+            },
+            onPrevClick() {
+                this.showPrev();
+                this.stopInterval();
+                this.createInterval();
+            },
+
+            selectImage(activeIndexImg) {
+                this.activeIndex = activeIndexImg;
+            }
         }
     }
-});
+);
 
 
 
